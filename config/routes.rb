740d247f33b1793f829c5143477f672518babce4
Batuhan Özcan <batuhan.ozcan@bil.omu.edu.tr>
2021-05-25 03:26:50 +0300

    add login functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root 'welcome#index'
-  get 'signup', to: 'users#new'
   resources :users, except: [:new]
-  # get 'login', to: 'sessions#new'
-  # post 'login', to: 'sessions#create'
-  # delete 'logout', to: 'sessions#destroy'
+  get 'signup', to: 'users#new'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end
